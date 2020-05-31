a505189c03e9c60eec2d7642ccc30f0234af5287
danmgs <dnlnguyen@gmail.com>
2020-05-31 10:08:33 +0200

    first commit

---
+++ src/SimpleAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
